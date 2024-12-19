c8214a8e02034bda197ec45c768ebbb9d0875a23
Gitts254 <63031602+Gitts254@users.noreply.github.com>
2024-12-19 09:47:53 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     alert('Welcome to My First Website!');
 });
+
+    
+<script>
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+</script>
